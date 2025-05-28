df69340341cad860c76114dfc0f20072939a4ae3
paolo9688 <paolo.pz1@gmail.com>
2025-05-28 12:23:03 +0200

    created CRUD operations for entity Studente

--- src/main/java/com/example/jpa_relazioni/OneToOne/controller/StudenteController.java
+++ src/main/java/com/example/jpa_relazioni/OneToOne/controller/StudenteController.java
@@ -1,9 +1,13 @@
 package com.example.jpa_relazioni.OneToOne.controller;
 
+import com.example.jpa_relazioni.OneToOne.entity.Studente;
 import com.example.jpa_relazioni.OneToOne.service.StudenteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/studenti")
@@ -11,4 +15,36 @@ public class StudenteController {
 
     @Autowired
     private StudenteService studenteService;
+
+    // crea un nuovo studente:
+    @PostMapping("/create-studente/")
+    public ResponseEntity<Studente> addStudente(@RequestBody Studente studente) {
+        Studente studenteToAdd = studenteService.addStudente(studente);
+        return ResponseEntity.ok(studenteToAdd);
+    }
+
+    // ottieni una lista di tutti gli studenti:
+    @GetMapping("/find-studenti")
+    public ResponseEntity<List<Studente>> getStudenti() {
+        List<Studente> studentiToFind = studenteService.getAllStudenti();
+        return ResponseEntity.ok(studentiToFind);
+    }
+
+    // ottieni uno studente per id:
+    @GetMapping("/find-studente/{id}")
+    public Optional<Studente> getStudenteById(@PathVariable Long id) {
+        Optional<Studente> studenteOptional = studenteService.getStudenteById(id);
+        return studenteOptional;
+    }
+
+    // cancella uno studente dal database:
+    @DeleteMapping("/delete-studente/{id}")
+    public ResponseEntity<Optional<Studente>> deleteStudente(@PathVariable Long id) {
+        Optional<Studente> studenteToDelete = studenteService.deleteStudente(id);
+
+        if (studenteToDelete.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(studenteToDelete);
+    }
 }
--- src/main/java/com/example/jpa_relazioni/OneToOne/service/StudenteService.java
+++ src/main/java/com/example/jpa_relazioni/OneToOne/service/StudenteService.java
@@ -1,12 +1,47 @@
 package com.example.jpa_relazioni.OneToOne.service;
 
+import com.example.jpa_relazioni.OneToOne.entity.Studente;
+import com.example.jpa_relazioni.OneToOne.repository.IndirizzoRepository;
 import com.example.jpa_relazioni.OneToOne.repository.StudenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudenteService {
 
     @Autowired
     private StudenteRepository studenteRepository;
+
+    @Autowired
+    private IndirizzoRepository indirizzoRepository;
+
+    // crea un nuovo studente:
+    public Studente addStudente(Studente studente) {
+        return studenteRepository.save(studente);
+    }
+
+    // ottieni una lista di tutti gli studenti:
+    public List<Studente> getAllStudenti() {
+        return studenteRepository.findAll();
+    }
+
+    // ottieni uno studente per id:
+    public Optional<Studente> getStudenteById(Long id) {
+        return studenteRepository.findById(id);
+    }
+
+    // cancella uno studente dal database:
+    public Optional<Studente> deleteStudente(Long id) {
+        Optional<Studente> studenteOptional = studenteRepository.findById(id);
+
+        if (studenteOptional.isPresent()) {
+            studenteRepository.deleteById(id);
+            return studenteOptional;
+        } else {
+            return Optional.empty();
+        }
+    }
 }
