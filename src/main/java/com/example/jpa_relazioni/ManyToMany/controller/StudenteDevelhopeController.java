cad62b33f2130074996d8b27d8a5038a7c86bf24
paolo9688 <paolo.pz1@gmail.com>
2025-06-04 23:28:26 +0200

    created new class ResourceNotFoundException, fixed CRUD methods for StudenteDevelhope

--- src/main/java/com/example/jpa_relazioni/ManyToMany/controller/StudenteDevelhopeController.java
+++ src/main/java/com/example/jpa_relazioni/ManyToMany/controller/StudenteDevelhopeController.java
@@ -3,10 +3,12 @@ package com.example.jpa_relazioni.ManyToMany.controller;
 import com.example.jpa_relazioni.ManyToMany.entity.StudenteDevelhope;
 import com.example.jpa_relazioni.ManyToMany.service.StudenteDevelhopeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/studenti-dev")
@@ -22,10 +24,45 @@ public class StudenteDevelhopeController {
         return ResponseEntity.ok(studenteToAdd);
     }
 
+    // ritorna uno studente per id:
+    @GetMapping("/find-studente/{id}")
+    public ResponseEntity<Optional<StudenteDevelhope>> getStudenteById(@PathVariable Long id) {
+        Optional<StudenteDevelhope> studenteOptional = studenteDevelhopeService.getStudenteById(id);
+
+        if (studenteOptional.isPresent()) {
+            return ResponseEntity.ok(studenteOptional);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
     // ritorna la lista di tutti gli studenti:
     @GetMapping("/find-studenti")
     public ResponseEntity<List<StudenteDevelhope>> getStudenti() {
-        List<StudenteDevelhope> studenteToFind = studenteDevelhopeService.getStudenti();
-        return ResponseEntity.ok(studenteToFind);
+        List<StudenteDevelhope> studentiDevelhope = studenteDevelhopeService.getStudenti();
+        return ResponseEntity.ok(studentiDevelhope);
+    }
+
+    // aggiorna uno studente esistente:
+    /*
+    * Grazie all'annotazione @ResponseStatus sulla ResourceNotFoundException,
+    * il controller può essere semplificato notevolmente.
+    * Non abbiamo più bisogno di un if/else per controllare il valore di ritorno null,
+    * Spring si occuperà di mappare l'eccezione allo status HTTP 404.
+    */
+    @PutMapping("/update-studente/{id}")
+    public ResponseEntity<StudenteDevelhope> updateStudente(@PathVariable Long id,
+                                                            @RequestBody StudenteDevelhope studente) {
+        StudenteDevelhope updatedStudente = studenteDevelhopeService.updateStudente(id, studente);
+        return new ResponseEntity<>(updatedStudente, HttpStatus.OK);
+    }
+
+    // cancella uno studente per id (cancellazione fisica):
+    @DeleteMapping("/delete-studente/{id}")
+    public ResponseEntity<String> deleteStudente(@PathVariable Long id) {
+        boolean deleted = studenteDevelhopeService.deleteStudente(id);
+        if (deleted) {
+            return new ResponseEntity<>("Studente con ID " + id + " cancellato con successo.", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Studente con ID " + id + " non trovato.", HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/example/jpa_relazioni/ManyToMany/service/StudenteDevelhopeService.java
+++ src/main/java/com/example/jpa_relazioni/ManyToMany/service/StudenteDevelhopeService.java
@@ -1,11 +1,14 @@
 package com.example.jpa_relazioni.ManyToMany.service;
 
+import com.example.jpa_relazioni.ManyToMany.entity.Corso;
 import com.example.jpa_relazioni.ManyToMany.entity.StudenteDevelhope;
+import com.example.jpa_relazioni.ManyToMany.exception.ResourceNotFoundException;
 import com.example.jpa_relazioni.ManyToMany.repository.StudenteDevelhopeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudenteDevelhopeService {
@@ -18,8 +21,35 @@ public class StudenteDevelhopeService {
         return studenteDevelhopeRepository.save(studenteDevelhope);
     }
 
+    // ritorna uno studente per id:
+    public Optional<StudenteDevelhope> getStudenteById(Long id) {
+        return studenteDevelhopeRepository.findById(id);
+    }
+
     // ritorna la lista di tutti gli studenti:
     public List<StudenteDevelhope> getStudenti() {
         return studenteDevelhopeRepository.findAll();
+    }
+
+    // aggiorna uno studente esistente (da testare):
+    public StudenteDevelhope updateStudente(Long id, StudenteDevelhope studenteDetails) {
+        Optional<StudenteDevelhope> studenteOptional = studenteDevelhopeRepository.findById(id);
+        if (studenteOptional.isPresent()) {
+            StudenteDevelhope existingStudente = studenteOptional.get();
+            existingStudente.setNome(studenteDetails.getNome());
+            existingStudente.setCorsi(studenteDetails.getCorsi());
+            return studenteDevelhopeRepository.save(existingStudente);
+        } else {
+            throw new ResourceNotFoundException("Studente con ID " + id + " non trovato.");
+        }
+    }
+
+    // cancella uno studente per id:
+    public boolean deleteStudente(Long id) {
+        if (studenteDevelhopeRepository.existsById(id)) {
+            studenteDevelhopeRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
